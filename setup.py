8e67e72abab4bb7ee5f9fbf2945c7975141a511d
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2015-09-04 21:28:29 +0200

    Windows install generator

--- setup.py
+++ setup.py
@@ -1,32 +1,59 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+class Executable:
+    def __init__(self, *args, **kwargs):
+        print("Foo")
+        pass
+try:
+    from cx_Freeze import setup, Executable
+except:
+    pass
 import sys
 
-install_requires = ['sqlalchemy',
-                    'PySide',
-                    'jinja2',
-                    'pyquery',
-                    'weasyprint',
-                    'appdirs']
 
 
+base = None
 if sys.platform.startswith("win32"):
     install_requires = ['sqlalchemy',
                         'PySide',
                         'jinja2',
                         'pyquery',
                         'appdirs']
+    base = "Win32GUI"
+    build_exe_options = {}
+    setup(
+        name = "pySpellbook",
+        version = "0.7.1",
+        packages = find_packages('src'),
+        package_dir = {'':'src'},
+        scripts = ["src/pySpellbook-qt4"],
+        package_data = {'pySpellbook':['templates/html/resources/*','templates/html/template.html']},
+        entry_points={
+            'console_scripts': [
+                'pySpellbook = pySpellbook.qt:run_pyspellbook'
+            ]},
+        install_requires = install_requires,
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("src/pySpellbook-qt4", base=base)]
+    )
+else:
+    install_requires = ['sqlalchemy',
+                        'PySide',
+                        'jinja2',
+                        'pyquery',
+                        'weasyprint',
+                        'appdirs']
+    setup(
+        name = "pySpellbook",
+        version = "0.7.1",
+        packages = find_packages('src'),
+        package_dir = {'':'src'},
+        package_data = {'pySpellbook':['templates/html/resources/*','templates/html/template.html']},
+        entry_points={
+            'console_scripts': [
+                'pySpellbook = pySpellbook.qt:run_pyspellbook'
+            ]},
+        install_requires = install_requires,
+    )
 
 
-setup(
-    name = "pySpellbook",
-    version = "0.7",
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
-    package_data = {'pySpellbook':['templates/html/resources/*','templates/html/template.html']},
-    entry_points={
-          'console_scripts': [
-              'pySpellbook = pySpellbook.qt:run_pyspellbook'
-          ]},
-    install_requires = install_requires
-)
